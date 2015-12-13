bfff3d039a660518226d671d2d9cfeeafd403550
Utilisateur <Utilisateur@PCMICRO-OHRS1GC>
2015-12-13 04:47:32 +0100

    Création du projet AuthenticationModule

---
+++ src/main/java/com/thamadjibo/generic/AuthenticationModule/App.java
@@ -0,0 +1,13 @@
+package com.thamadjibo.generic.AuthenticationModule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
